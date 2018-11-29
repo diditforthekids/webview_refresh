2d29a4ad0605cb8dd485ad7319be4dffbcbbf213
Jmako <jmakoroff@gmail.com>
2018-11-28 21:29:44 -0600

    Initial Commit

---
+++ WebDisplay_Refresh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebDisplay_Refresh
+//
+//  Created by Jeff Makoroff on 11/28/18.
+//  Copyright © 2018 Jeff Makoroff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
